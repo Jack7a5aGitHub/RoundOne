457bfbb3c82a3b3301f09b46580f566a0d6e3363
Jack7a5aGitHub <jack7a5a1993@icloud.com>
2017-09-10 14:35:57 +0900

    test 9/10

---
+++ Round One/NavigationController.swift
@@ -0,0 +1,29 @@
+//
+//  NavigationController.swift
+//  Round One
+//
+//  Created by Jack Wong on 2017/08/22.
+//  Copyright © 2017年 None. All rights reserved.
+//
+
+import UIKit
+import ChameleonFramework
+
+class NavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        navigationBar.barTintColor = FlatBlue()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+   
+}
--- Round One/ViewController.swift
+++ Round One/ViewController.swift
@@ -6,14 +6,56 @@
 //  Copyright © 2017年 None. All rights reserved.
 //
 
+import ChameleonFramework
 import UIKit
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITabBarDelegate{
     
     @IBOutlet weak var collectionView: UICollectionView!
     
+    @IBOutlet weak var tabBarView: UITabBar!
     
+   
+    @IBOutlet weak var tabBarView2: UITabBar!
+        
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        
+        switch  item.tag {
+        case 0:
+            performSegue(withIdentifier: "second", sender: self)
+        case 1:
+            let url = URL(string: "https://www.round1.co.jp")
+            UIApplication.shared.open(url!, options: [:], completionHandler: nil)
+        case 2:
+            let url = URL(string: "https://www.google.com")
+            UIApplication.shared.open(url!, options: [:], completionHandler: nil)
+        case 3:
+            break
+        case 4:
+            if tabBarView2.isHidden == true {
+                tabBarView2.isHidden = false
+            }else {
+                tabBarView2.isHidden = true
+            }
+        default:
+            break
+        }
+        /*
+        if item.tag == 0 {
+            performSegue(withIdentifier: "second", sender: self)
+         
+        let url = URL(string: "https://www.round1.co.jp")
+        UIApplication.shared.open(url!, options: [:], completionHandler: nil)
+ 
+        }else {
+            return
+        }
+ */
+    }
+    
+    
+  
     var pictures = ["megapack","cashback","bus","challengebo","cashback","bus","megapack","cashback","bus", "challengebo"]
     var nib = UINib(nibName: "CellCollectionCollectionViewCell", bundle: nil)
     
@@ -22,14 +64,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         setupNavgationBar()
+       //setupTabBarItem()
+      tabBarView.delegate = self
+        tabBarView2.isHidden = true
         collectionView.register(nib,  forCellWithReuseIdentifier: "cell")
         if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
             flowLayout.estimatedItemSize = CGSize(width: 1, height: 1)
+            collectionView.backgroundColor = FlatGrayDark()
+         
         }
         //collectionView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tap)))
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        welcomeAlert()
+        dismiss(animated: <#T##Bool#>, completion: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -37,6 +88,7 @@ class ViewController: UIViewController {
     
     
     func setupNavgationBar(){
+       
         navigationItem.title = " Round 1 "
         //let leftButton = UIBarButtonItem(image: UIImage(named: "homeIcon"), style: .plain, target: nil, action: nil)
         // let rightButton = UIBarButtonItem(image: UIImage(named: "update"), style: .plain, target: nil, action: nil)
@@ -53,7 +105,24 @@ class ViewController: UIViewController {
         self.navigationItem.setLeftBarButton(item1, animated: true)
         self.navigationItem.setRightBarButton(item2, animated: true)
     }
-    
+    func setupTabBarItem(){
+        
+        var tabBarViewItem = [UITabBarItem]()
+        let tabBarItem1 = (self.tabBarView.items![0]) as UITabBarItem
+        tabBarItem1.title = "レーン予約88"
+        tabBarViewItem.append(tabBarItem1)
+        let tabBarItem2 = (self.tabBarView.items![1]) as UITabBarItem
+        tabBarItem2.title = "店舗検索"
+         tabBarViewItem.append(tabBarItem2)
+        
+        
+    }
+    func welcomeAlert(){
+      let alert = UIAlertController(title: "Welcome", message: "RoundOne", preferredStyle: .alert)
+    let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+}
 }
 
 
@@ -73,13 +142,13 @@ extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDe
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let webView = UIWebView()
-        let url = NSURL(string: "www.yahoo.com.hk")!
-        let request = URLRequest(url: url as URL, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 10)
+       
+        let url = NSURL(string: "https://www.yahoo.com.hk")!
+       
         if pictures[indexPath.item] == "megapack" {
             
-            // UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
-            webView.loadRequest(request)
+             UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
+            print(url)
             print("hihih")
             
         }else {
@@ -87,42 +156,5 @@ extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDe
         }
     }
    
-    
-   /*
-    func tap(){
-        print("test")
-        
-    }
-    */
-    /*
-     func collectionView(collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath: NSIndexPath, withWidth: CGFloat) -> CGFloat {
-     
-     let picture = UIImage(named: pictures[indexPath.item])
-     let boundRect = CGRect(x: 0, y: 0, width: withWidth, height: CGFloat(MAXFLOAT))
-     let rect = AVMakeRect(aspectRatio: (picture?.size)!, insideRect: boundRect)
-     return rect.size.height
-     
-     
-     }
-     
-     func collectionView(collectionView: UICollectionView, heightForAnnotationAtIndexPath indexPath: NSIndexPath, withWidth width: CGFloat) -> CGFloat {
-     let annotationPadding = CGFloat(4)
-     let annotationHeaderHeight = CGFloat(17)
-     let font = UIFont(name: " Hihi I am Tester ", size: 10)
-     let pic = pictures[indexPath.item]
-     let commentHeight = pic.heightForComment(font: font!, width: width)
-     let height = annotationPadding + annotationHeaderHeight + commentHeight + annotationPadding
-     return height
-     }
-     
-     func heightForComment(font: UIFont, width: CGFloat) -> CGFloat {
-     let rect = NSString(string: comment!).boundingRect(with: CGSize(width: width, height: CGFloat(MAXFLOAT)), options: .usesLineFragmentOrigin, attributes: [NSFontAttributeName: font], context: nil)
-     return ceil(rect.height)
-     }
-     */
-    
-    
-    
-    
     
 }
